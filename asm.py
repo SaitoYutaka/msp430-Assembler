b8e052e4baf4a27be9bb4c56d4de7141ca104415
Saito Yutaka <melody.pea@gmail.com>
2011-02-04 07:22:56 +0900

    add label function

--- asm.py
+++ asm.py
@@ -5,6 +5,27 @@ import msp430x2xx
 def usage():
     print('usage: asm.py [file]')
 
+def deletenl(line):
+    line = line.strip('\n')
+    line = line.lstrip()
+    return line
+
+def getLabel(s):
+    if s.find(' ') == -1: return s
+    else: return s[0:s.find(' ')]
+
+def GetAllLabel(f):
+    dLabel = {}
+    dummyval = 0
+    for line in f:
+        line = deletenl(line)
+        if line[0] == ':':
+            tmp = getLabel(line)
+            dLabel[tmp[1:]] = dummyval
+            dummyval += 1
+            continue
+    return dLabel
+
 if len(sys.argv) == 1:
     usage()
     sys.exit()
@@ -16,32 +37,62 @@ except:
     usage()
     sys.exit()
 
-dLabel = {}
-lineno = 1    
+lineno = 1   
 x = msp430x2xx.MSP430x2xx()
-for line in f:
-    if line[0] == ':':
-        dLabel[line] = None
-        continue
+dLabel = GetAllLabel(f)
 f.seek(0)
 
+tmparr = []
+addrnum = 0xf800
 for line in f:
-    line = line.strip('\n')
-    line = line.lstrip()
+    line = deletenl(line)
+    isLabel = ''
+    source = ''
 
-    if line == '' or line[0] == ';' or line[0] == ':' : continue
-    opcode = x.asm(line)
+
+    if line == '' or line[0] == ';': continue
+
+    if line[0] == ':' :
+        tmp = getLabel(line)
+        dLabel[tmp[1:]] = addrnum
+        
+        if line.replace(tmp,'') == '':continue
+        else: 
+            line = line.replace(tmp,'')
+            line = deletenl(line)
+
+    for label in dLabel.keys():
+        if label in line:
+            source = line.replace(label,str(dLabel[label]))
+            isLabel = label
+
+    if isLabel == '': source = line
+
+    opcode = x.asm(source)
 
     if opcode[0] == -1:
         print(opcode[1],end='')
         print(' line:' + str(lineno))
         sys.exit()
 
-    print ('%-30s' % line,end='')
-    for byte in opcode:
-        print ("0x%04x " % byte,end='')
-    print()
+    tmparr.append([line,isLabel,opcode,addrnum])
+    for byte in opcode: addrnum += 4
     lineno += 1
 
-print(dLabel.keys())
+SOURCE  = 0
+LABEL   = 1
+OPCODE  = 2
+ADDRESS = 3
+for asminfo in tmparr:
+    print ('%-30s' % asminfo[SOURCE],end='')
+    print ("0x%04x " % asminfo[ADDRESS],end='')
+    if asminfo[LABEL] != '': # label?
+        opcode = x.asm(asminfo[SOURCE].replace(asminfo[LABEL],str(dLabel[asminfo[LABEL]])))
+        for byte in opcode:
+            print ("0x%04x " % byte,end='')
+    else:
+        for byte in asminfo[OPCODE]:
+            print ("0x%04x " % byte,end='')
+    print()
+print(dLabel)
 f.close()
